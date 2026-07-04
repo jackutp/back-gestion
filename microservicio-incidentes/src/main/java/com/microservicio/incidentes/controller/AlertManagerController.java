08c8c7c01d507e5ad048580ae3e54f5653ccfa47
italo <riositalo2@gmail.com>
2026-07-04 05:33:11 -0500

    Minor fixes

--- microservicio-incidentes/src/main/java/com/microservicio/incidentes/controller/AlertManagerController.java
+++ microservicio-incidentes/src/main/java/com/microservicio/incidentes/controller/AlertManagerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/incidentes")
+@RequestMapping("/api/incidentes/alert")
 @RequiredArgsConstructor
 public class AlertManagerController {
     private final AlertManagerService alertManagerService;
